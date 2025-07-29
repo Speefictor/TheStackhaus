5345a8d4abb2cd55cadbbbc4a35f8ddf7f31b28e
Cyril Gong <gongcunji@gmail.com>
2025-07-29 06:24:44 +0000

    Use the "Cardinal Photo" font as the preferred font for all content titl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Cardinal&family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </head>
